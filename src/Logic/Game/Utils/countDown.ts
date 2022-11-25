c94eeaa30cb6e59aff4b400b410c1fd2f059bb5d
bernat-ferrer <bernatferrerm@gmail.com>
2022-11-25 17:40:36 +0100

    fix countdown

--- src/Logic/Game/Utils/countDown.ts
+++ src/Logic/Game/Utils/countDown.ts
@@ -1,11 +1,9 @@
 import {Ref} from "vue";
 
-const arrayCountDown = ["..","Loading..","6","5", "4", "3", "2", "1", "0", "GO", "!"]; // count down array for beginning the game
+const arrayCountDown = ["Loading","5", "4", "3", "2", "1", "0", "GO"]; // count down array for beginning the game
 
 
 const countDown = (countDownText: Ref, callback: Function) => {
-
-
     let counter = 0
     let timer = setInterval(() => {
 
